ffe65e97b1bcc5adc134e23848dd3b133315b490
Guy Ezra <guyez1@ac.sce.ac.il>
2024-12-29 15:40:43 +0200

    test

---
+++ verdict/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div className="align-items-center">
+      <h1 >Hi</h1>
+      <h1></h1>
+
+      <Button>Click Me</Button>
+    </div>
+  );
+}
